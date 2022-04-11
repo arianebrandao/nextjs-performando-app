97318a33e6dd8a84c9d82237710dbd6e18d288d1
Ariane Brandão <ariane_mad@hotmail.com>
2022-04-11 17:09:16 -0300

    add useMemo()

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,21 +1,35 @@
+import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
-    results: Array<{
-        id: number;
-        price: number;
-        title: string;
-    }>
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+  }>;
 }
 
-export function SearchResults({results}: SearchResultsProps) {
-    return (
-        <div>
-            { results.map(product => {
-                return (
-                    <ProductItem key={product.id} product={product}/>
-                );
-            }) }
-        </div>
-    )
+export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+
+  return (
+    <div>
+      <h2>{totalPrice}</h2>
+      {results.map((product) => {
+        return <ProductItem key={product.id} product={product} />;
+      })}
+    </div>
+  );
 }
+
+/**
+ * useMemo() é usado:
+ * 1. Calculos pesados
+ * 2. Igualdade referencial (quando repassa a informação para um componente filho)
+ */
+
+
