d3f1cd3ed8de0ef2f8699145a262022faeeab70f
Ariane Brandão <ariane_mad@hotmail.com>
2022-04-11 19:08:52 -0300

    add bundle analyzer

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -24,28 +24,29 @@ export function SearchResults({
   //     }, 0);
   //   }, [results]);
 
-  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
-    return (
-      <div key={key} style={style}>
-        <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
-      </div>
-    );
-  };
+  // List React Virtualized
+  // const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+  //   return (
+  //     <div key={key} style={style}>
+  //       <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+  //     </div>
+  //   );
+  // };
 
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      <List
+      {/* <List
         height={300}
         rowHeight={30}
         width={900}
         overscanRowCount={5}
         rowCount={results.length}
         rowRenderer={rowRenderer}
-      />
+      /> */}
 
-      {/* {results.map((product) => {
+      {results.map((product) => {
         return (
           <ProductItem
             key={product.id}
@@ -53,7 +54,7 @@ export function SearchResults({
             onAddToWishlist={onAddToWishlist}
           />
         );
-      })} */}
+      })}
     </div>
   );
 }
